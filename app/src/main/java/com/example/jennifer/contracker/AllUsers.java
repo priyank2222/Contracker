0865469f1ae20317967f6ffad4e316049be64683
zzzke <zhaok321@gmail.com>
2018-04-29 18:15:22 -0400

    Update AllUsers.java
    all users comment added

--- app/src/main/java/com/example/jennifer/contracker/AllUsers.java
+++ app/src/main/java/com/example/jennifer/contracker/AllUsers.java
@@ -2,6 +2,7 @@ package com.example.jennifer.contracker;
 
 /**
  * Created by kezhao on 4/8/18.
+ * Model class
  */
 
 class AllUsers {
@@ -11,36 +12,44 @@ class AllUsers {
     public String  username;
     public String expertise;
 
+
+    // constructor
     public AllUsers(){
 
     }
-
+    // constructor
     public AllUsers(String user_image, String username, String expertise) {
         this.user_image = user_image;
         this.username = username;
         this.expertise = expertise;
     }
 
+    //getter and setter methods
     public String getUser_image() {
         return user_image;
     }
 
+    //getter and setter methods
     public void setUser_image(String user_image) {
         this.user_image = user_image;
     }
 
+    //getter and setter methods
     public String getUsername() {
         return username;
     }
 
+    //getter and setter methods
     public void setUsername(String username) {
         this.username = username;
     }
 
+    //getter and setter methods
     public String getExpertise() {
         return expertise;
     }
 
+    //getter and setter methods
     public void setExpertise(String expertise) {
         this.expertise = expertise;
     }
