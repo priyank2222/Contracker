3433f77f7e6770e7814bcdccc11a70b20adb1a2c
zzzke <zhaok321@gmail.com>
2018-04-29 18:35:58 -0400

    Update Comments.java

--- app/src/main/java/com/example/jennifer/contracker/Comments.java
+++ app/src/main/java/com/example/jennifer/contracker/Comments.java
@@ -10,12 +10,12 @@ public class Comments {
     private String senderusername;
     private String datestamp;
 
-
+    // constructor
     public Comments() {
 
 
     }
-
+    // constructor
     public Comments(String commentbody, String senderusername, String datestamp) {
         this.commentbody = commentbody;
         this.senderusername = senderusername;
@@ -23,24 +23,30 @@ public class Comments {
 
     }
 
+    //getter and setter methods
     public String getCommentbody() {
         return commentbody;
     }
 
+    //getter and setter methods
     public void setCommentbody(String commentbody) {
         this.commentbody = commentbody;
     }
 
+    //getter and setter methods
     public String getSenderusername() {
         return senderusername;
     }
 
+    //getter and setter methods
     public void setSenderusername(String senderusername) {
         this.senderusername = senderusername;
     }
 
+    //getter and setter methods
     public String getDatestamp(){ return datestamp; }
 
+    //getter and setter methods
     public void setDatestamp(String datestamp){ this.datestamp = datestamp; }
 
 }
