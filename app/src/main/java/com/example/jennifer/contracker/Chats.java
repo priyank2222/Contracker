a2fe122a947756ee28cbedb7f09518b97548a8dd
zzzke <zhaok321@gmail.com>
2018-04-29 18:22:21 -0400

    Update Chats.java

--- app/src/main/java/com/example/jennifer/contracker/Chats.java
+++ app/src/main/java/com/example/jennifer/contracker/Chats.java
@@ -7,18 +7,22 @@ package com.example.jennifer.contracker;
 public class Chats
 {
     private String experience;
+
+    // constructor
     public Chats(){
 
     }
-
+    //getter and setter methods
     public Chats(String experience) {
         this.experience = experience;
     }
 
+    //getter and setter methods
     public String getExperience() {
         return experience;
     }
 
+    //getter and setter methods
     public void setExperience(String experience) {
         this.experience = experience;
     }
