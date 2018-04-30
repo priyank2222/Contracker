37f39a6ed50715cb8a4f6aa6d6c8a98b37a1eb45
zzzke <zhaok321@gmail.com>
2018-04-30 00:43:23 -0400

    Update Posts.java

--- app/src/main/java/com/example/jennifer/contracker/Posts.java
+++ app/src/main/java/com/example/jennifer/contracker/Posts.java
@@ -2,6 +2,7 @@ package com.example.jennifer.contracker;
 
 /**
  * Created by kezhao on 4/11/18.
+ * Model class
  */
 
 public class Posts {
@@ -26,42 +27,52 @@ public class Posts {
     }
 
 
+    //getter and setter methods
     public String getJob_title() {
         return job_title;
     }
 
+    //getter and setter methods
     public void setJob_title(String job_title) {
         this.job_title = job_title;
     }
 
+    //getter and setter methods
     public String getJob_description() {
         return job_description;
     }
 
+    //getter and setter methods
     public void setJob_description(String job_description) {
         this.job_description = job_description;
     }
 
+    //getter and setter methods
     public String getJob_location() {
         return job_location;
     }
 
+    //getter and setter methods
     public void setJob_location(String job_location) {
         this.job_location = job_location;
     }
 
+    //getter and setter methods
     public String getService_category() {
         return service_category;
     }
 
+    //getter and setter methods
     public void setService_category(String service_category) {
         this.service_category = service_category;
     }
 
+    //getter and setter methods
     public String getClose_bidding() {
         return close_bidding;
     }
 
+    //getter and setter methods
     public void setClose_bidding(String close_bidding) {
         this.close_bidding = close_bidding;
     }
