c9d1914946312d470cec95645d313d0bab37b9bd
zzzke <zhaok321@gmail.com>
2018-04-30 00:20:05 -0400

    Update History.java

--- app/src/main/java/com/example/jennifer/contracker/History.java
+++ app/src/main/java/com/example/jennifer/contracker/History.java
@@ -10,11 +10,11 @@ class History
     private String job_rating,job_title,job_description,job_location,service_category;
 
 
-
+    // constructor
     public History(){
 
     }
-
+    // constructor
     public History(String date, String job_rating, String job_title, String job_description, String job_location, String service_category) {
         this.date = date;
         this.job_rating = job_rating;
@@ -24,50 +24,62 @@ class History
         this.service_category = service_category;
     }
 
+    //getter and setter methods
     public String getDate() {
         return date;
     }
 
+    //getter and setter methods
     public void setDate(String date) {
         this.date = date;
     }
 
+    //getter and setter methods
     public String getJob_rating() {
         return job_rating;
     }
 
+    //getter and setter methods
     public void setJob_rating(String job_rating) {
         this.job_rating = job_rating;
     }
 
+    //getter and setter methods
     public String getJob_title() {
         return job_title;
     }
 
+    //getter and setter methods
     public void setJob_title(String job_title) {
         this.job_title = job_title;
     }
 
+    //getter and setter methods
     public String getJob_description() {
         return job_description;
     }
 
+    //getter and setter methods
     public void setJob_description(String job_description) {
         this.job_description = job_description;
     }
 
+    //getter and setter methods
     public String getJob_location() {
         return job_location;
     }
 
+    //getter and setter methods
     public void setJob_location(String job_location) {
         this.job_location = job_location;
     }
 
+    //getter and setter methods
     public String getService_category() {
         return service_category;
     }
 
+    //getter and setter methods
     public void setService_category(String service_category) {
         this.service_category = service_category;
     }
