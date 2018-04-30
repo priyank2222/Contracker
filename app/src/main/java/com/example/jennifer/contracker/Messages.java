3233a1d26fc1b832cb7fbbad337905dd74b45181
zzzke <zhaok321@gmail.com>
2018-04-30 00:37:44 -0400

    Update Messages.java

--- app/src/main/java/com/example/jennifer/contracker/Messages.java
+++ app/src/main/java/com/example/jennifer/contracker/Messages.java
@@ -2,6 +2,7 @@ package com.example.jennifer.contracker;
 
 /**
  * Created by kezhao on 4/17/18.
+ * Model class
  */
 
 public class Messages
@@ -11,10 +12,12 @@ public class Messages
     private boolean seen;
     private String from;
 
+    // constructor
     public Messages() {
 
     }
 
+    // constructor
     public Messages(String message, String type, long time, boolean seen, String from) {
         this.message = message;
         this.type = type;
@@ -23,42 +26,52 @@ public class Messages
         this.from = from;
     }
 
+    //getter and setter methods
     public String getMessage() {
         return message;
     }
 
+    //getter and setter methods
     public void setMessage(String message) {
         this.message = message;
     }
 
+    //getter and setter methods
     public String getType() {
         return type;
     }
 
+    //getter and setter methods
     public void setType(String type) {
         this.type = type;
     }
 
+    //getter and setter methods
     public long getTime() {
         return time;
     }
 
+    //getter and setter methods
     public void setTime(long time) {
         this.time = time;
     }
 
+    //getter and setter methods
     public boolean isSeen() {
         return seen;
     }
 
+    //getter and setter methods
     public void setSeen(boolean seen) {
         this.seen = seen;
     }
 
+    //getter and setter methods
     public String getFrom() {
         return from;
     }
 
+    //getter and setter methods
     public void setFrom(String from) {
         this.from = from;
     }
